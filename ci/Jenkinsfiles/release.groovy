efbd0985048962e32f0760d987b573fe9ea3c4d2
Antoine Taillefer <ataillefer@nuxeo.com>
2024-09-02 22:42:03 +0200

    NXBT-3615: Upgrade shared library

--- ci/Jenkinsfiles/release.groovy
+++ ci/Jenkinsfiles/release.groovy
@@ -20,7 +20,7 @@
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-library identifier: "platform-ci-shared-library@v0.0.28"
+library identifier: "platform-ci-shared-library@v0.0.38"
 
 pipeline {
   agent {
