e2e4704d0c80b623c22cecf6d0fc3c3e124aff19
Kevin Leturc <kevin.leturc@hyland.com>
2024-09-09 10:03:44 +0200

    NXBT-3821: Upgrade shared library to get a fix

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -17,7 +17,7 @@
 *     Kevin Leturc <kevin.leturc@hyland.com>
 *     Antoine Taillefer <antoine.taillefer@hyland.com>
 */
-library identifier: "platform-ci-shared-library@v0.0.39"
+library identifier: "platform-ci-shared-library@v0.0.40"
 
 String getCLIDSecret() {
   container('maven') {
--- ci/Jenkinsfiles/release.groovy
+++ ci/Jenkinsfiles/release.groovy
@@ -20,7 +20,7 @@
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-library identifier: "platform-ci-shared-library@v0.0.39"
+library identifier: "platform-ci-shared-library@v0.0.40"
 
 pipeline {
   agent {
