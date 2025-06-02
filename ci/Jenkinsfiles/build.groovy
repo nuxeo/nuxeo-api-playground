9523ce531ee45995ebc7a79b5f7a1ef37f593784
Kevin Leturc <kevin.leturc@hyland.com>
2025-06-02 12:43:16 +0200

    NXBT-3864: Update shared library

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -17,7 +17,7 @@
 *     Kevin Leturc <kevin.leturc@hyland.com>
 *     Antoine Taillefer <antoine.taillefer@hyland.com>
 */
-library identifier: "platform-ci-shared-library@v0.0.40"
+library identifier: "platform-ci-shared-library@v0.0.67"
 
 String getCLIDSecret() {
   container('maven') {
--- ci/Jenkinsfiles/release.groovy
+++ ci/Jenkinsfiles/release.groovy
@@ -20,7 +20,7 @@
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-library identifier: "platform-ci-shared-library@v0.0.40"
+library identifier: "platform-ci-shared-library@v0.0.67"
 
 pipeline {
   agent {
