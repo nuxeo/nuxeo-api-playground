f1b82f987d6341acc709c31405b0bafde3d8f831
Kevin Leturc <kevin.leturc@hyland.com>
2025-09-17 13:37:24 +0200

    NXBT-3906: Update shared library

--- ci/Jenkinsfiles/build.groovy
+++ ci/Jenkinsfiles/build.groovy
@@ -17,7 +17,7 @@
 *     Kevin Leturc <kevin.leturc@hyland.com>
 *     Antoine Taillefer <antoine.taillefer@hyland.com>
 */
-library identifier: "platform-ci-shared-library@v0.0.72"
+library identifier: "platform-ci-shared-library@v0.0.73"
 
 String getCLIDSecret(nuxeoVersion) {
   return nuxeoVersion.matches("^\\d+\\.\\d+(-SNAPSHOT|\\.\\d+)\$") ? 'instance-clid-preprod' : 'instance-clid'
